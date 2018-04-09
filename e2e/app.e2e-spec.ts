52de08193f8f504cafeb4ee63dc944302473840f
Craig Doremus <cdoremus@gmail.com>
2018-04-08 21:58:09 -0400

    Fleshed out e2e tests; all pass

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -11,4 +11,37 @@ describe('angular-youtube App', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
+
+  it('should display 10 rows in data table', () => {
+    page.navigateTo();
+    page.getTableRows().count().then(count => {
+      expect(count).toEqual(10);
+    });
+  });
+
+  it('should navigate to next page', () => {
+    page.navigateTo();
+    page.getNextButton().click().then(() => {
+      page.getPageRangeLabel().getText().then(label => {
+        // label is '11 - 20 of 71'
+        const dash = label.indexOf(' -');
+        expect(label.substring(0, dash)).toBe('11');
+      });
+    });
+  });
+
+  it('should navigate to next page and back to first page', () => {
+    page.navigateTo();
+    page.getNextButton().click().then(() => {
+      // back to first page
+      page.getPreviousButton().click().then(() => {
+        page.getPageRangeLabel().getText().then(label => {
+          // label is '1 - 10 of 71'
+          const dash = label.indexOf(' -');
+          expect(label.substring(0, dash)).toBe('1');
+        });
+      });
+    });
+  });
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,5 +1,10 @@
 import { browser, by, element } from 'protractor';
 
+export enum NavigationDirection {
+  NEXT = 'next',
+  PREVIOUS = 'previous'
+}
+
 export class AppPage {
   navigateTo() {
     return browser.get('/');
@@ -8,4 +13,25 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getTableRows() {
+    return element.all(by.css('.mat-row'));
+  }
+
+  getPageRangeLabel() {
+    return element(by.css('.mat-paginator-range-label'));
+  }
+
+  getNextButton() {
+    return this.getNavigationButton(NavigationDirection.NEXT);
+  }
+
+  getNavigationButton(direction: NavigationDirection) {
+    return element(by.css(`button.mat-paginator-navigation-${direction}`));
+  }
+
+  getPreviousButton() {
+    return this.getNavigationButton(NavigationDirection.PREVIOUS);
+  }
+
 }
